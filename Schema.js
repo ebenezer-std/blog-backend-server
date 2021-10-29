a5b43c75275803f761a3add47ee708cebe75a6dd
Ebenezer <eaanertey@st.ug.edu.gh>
2021-10-29 17:46:41 +0000

    Created TypeDefs

--- Schema.js
+++ Schema.js
@@ -1,1 +1,54 @@
 const {ApolloServer, gql}= require('apollo-server');
+
+const typeDefs = gql`
+
+type Post{
+    title: String!
+    id: ID
+    content: String!
+    date: String
+    author: Author
+    comments: [Comment]
+    likes: Int
+    unlikes: Int
+
+
+}
+
+type Comment{
+    name: String!
+    content: String!
+
+}
+
+type Author{
+    name: String!
+    email: String!
+    Hobbies: String
+}
+
+input PostInput{
+    title: String!
+    id: ID
+    content: String!
+    date: String
+    author: String
+
+}
+
+type Query{
+    posts: [Post!]!
+    post(title: String): Post
+    #post(author: String): Post
+    #authors: [Author]!
+
+     
+}
+
+type Mutation{
+    createPost(input: PostInput!): Post!
+
+}
+
+
+`
